072945d66f92af8aac24061f9de50bd9fab9d981
Tomas Rojo <tomasrojo21@hotmail.com>
2025-03-18 00:35:56 +0100

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Required for static export to GitHub Pages
+    basePath: '/pylaunch-site',
+    assetPrefix: '/pylaunch-site/',
+    images: {
+      unoptimized: true, // Disable optimization for static export
+    },
+  };
 
 export default nextConfig;
